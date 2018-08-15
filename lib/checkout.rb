05d13a0b324a8ea4ea6b980946628e77860a8f81
Harry <harryjp1995@gmail.com>
2018-08-15 14:09:29 +0100

    Refactors code.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,45 +1,55 @@
+require 'terminal-table'
 require_relative 'item'
 
 class Checkout
-  attr_reader :receipt
+  attr_reader :receipt, :log
 
   def initialize
     @receipt = []
-    @total = 0
   end 
 
   def return_item_price(item)
     "£#{item.price}"
   end
 
+  def print_receipt
+    table = Terminal::Table.new :headings => ['Item', 'Quantity'], :rows => items_with_quantity
+  end
+
   def scan(item, quantity=1)
-    already_scanned(item.name) ? @receipt[item_index(item.name)][:quantity] += quantity : @receipt << { name: item.name, price: item.price, quantity: quantity }
+    already_scanned?(item.name) ? update_quantity(item.name, quantity) : @receipt << { item: item, quantity: quantity }
   end
 
   def remove_item(item_name, quantity=1)
+    raise 'Item has not been scanned' if !already_scanned?(item_name)
     raise 'Scanned quantity is less than the quantity specified' if quantity_of(item_name) < quantity
-    @receipt[item_index(item_name)][:quantity] -= quantity
+    update_quantity(item_name, -quantity)
   end
 
   def total
-    @receipt.map { |item| item[:price] * item[:quantity] }.inject('+')
+    return 0 if @receipt.count == 0
+    @receipt.map { |entry| entry[:item].price * entry[:quantity] }.inject('+')
   end
 
   private
 
     def quantity_of(item_name)
-      @receipt.select { |item| item[:name] == item_name }.map { |i| i[:quantity] }.inject('+') 
+      @receipt.select { |entry| entry[:item].name == item_name }.map { |i| i[:quantity] }.inject('+') 
     end
 
-    def already_scanned(item_name)
-      @receipt.map { |item| item[:name] }.include?(item_name)
+    def items_with_quantity
+      @receipt.map { |entry| [entry[:item].name, entry[:quantity]] }
+    end
+
+    def already_scanned?(item_name)
+      @receipt.map { |entry| entry[:item].name }.include?(item_name)
     end
 
     def item_index(item_name)
-      @receipt.index { |item| item[:name] == item_name }
+      @receipt.index { |entry| entry[:item].name == item_name }
+    end
+
+    def update_quantity(item_name, quantity)
+      @receipt[item_index(item_name)][:quantity] += quantity
     end
 end
-
-# co = Checkout.new
-# apple = Item.new(:apple, 2)
-# orange = Item.new(:orange, 1.5)
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -15,23 +15,27 @@ describe Checkout do
 
     it 'scans an item, and adds one of it to the receipt by default' do
       subject.scan(apple)
-      expect(subject.receipt).to eq [{ name: :apple, price: 2, quantity: 1 }]
+      expect(subject.receipt).to eq [{ item: apple, quantity: 1 }]
     end
 
     it 'adds more than one of the item to the receipt if specified' do
       subject.scan(apple, 3)
-      expect(subject.receipt).to eq [{ name: :apple, price: 2, quantity: 3 }]
+      expect(subject.receipt).to eq [{ item: apple, quantity: 3 }]
     end
 
-    it 'adds more of an item if scanned later' do
+    it 'updates quantity of an item if scanned later' do
       subject.scan(apple, 3)
       subject.scan(orange, 3)
       subject.scan(apple, 3)
-      expect(subject.receipt).to eq [{ name: :apple, price: 2, quantity: 6 }, { name: :orange, price: 1.5, quantity: 3 }]
+      expect(subject.receipt).to eq [{ item: apple, quantity: 6 }, { item: orange, quantity: 3 }]
     end
   end
 
   describe '#total' do
+
+    it 'returns zero when nothing has been scanned' do
+      expect(subject.total).to eq 0
+    end
 
     it 'returns the total for items, when scanned individually' do
       subject.scan(apple)
@@ -55,12 +59,16 @@ describe Checkout do
 
       it 'allows you to remove item from the receipt, given its name' do
         subject.remove_item(:apple)
-        expect(subject.receipt).to eq [{ name: :apple, price: 2, quantity: 2 }, { name: :orange, price: 1.5, quantity: 3 }]
+        expect(subject.receipt).to eq [{ item: apple, quantity: 2 }, { item: orange, quantity: 3 }]
       end
   
       it 'allows you to remove a certain number of an item' do
         subject.remove_item(:apple, 2)
-        expect(subject.receipt).to eq [{ name: :apple, price: 2, quantity: 1 }, { name: :orange, price: 1.5, quantity: 3}]
+        expect(subject.receipt).to eq [{ item: apple, quantity: 1 }, { item: orange, quantity: 3}]
+      end
+
+      it 'raises an error if you try to remove an item that hasn\'t been scanned' do
+        expect{subject.remove_item(:banana)}.to raise_error('Item has not been scanned')
       end
   
       it 'raises an error if you try to remove more of an item than has been scanned' do
